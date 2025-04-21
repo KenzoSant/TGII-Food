0d39263b320c02ec4afb4d41f0934d4d9b2e356e
kenzosant <mateuskenzo_santos@hotmail.com>
2025-04-21 19:50:32 -0300

    commit-admm

--- admin/render-build.sh
+++ admin/render-build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-# Acessa o diretório admin se existir, senão assume que já está nele
-if [ -d "admin" ]; then
-  cd admin
-fi
-
-# Instala dependências com permissões adequadas
-npm install --unsafe-perm --legacy-peer-deps
-npm install @vitejs/plugin-react --save-dev
-
-# Configura permissões
+cd admin
+npm ci --unsafe-perm --legacy-peer-deps
 chmod -R 755 node_modules/.bin
-
-# Executa o build
 npm run build
